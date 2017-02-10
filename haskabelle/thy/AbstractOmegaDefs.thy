3c0e934e8bfaa38f20b0d157b212b3d7d3324db6
Artur Oliveira Gomes <artur@Firebird.local>
2017-02-10 13:36:23 +0000

    Yes! Proofs! I just wrote some proofs abstract x concrete implementation of Omega functions. Yes, they are renamed with the prefix “abs_” for abstract. We have to make it more abstract for sure! I’ll try something with sets.

--- haskabelle/thy/AbstractOmegaDefs.thy
+++ haskabelle/thy/AbstractOmegaDefs.thy
@@ -1,23 +1,14 @@
-section "Abstract Auxiliary Definitions for Omega functions"
+section "Auxiliary Definitions for Omega functions"
 
-theory AbstractOmegaDefs imports AST Prelude OmegaDefs begin
+theory AbstractOmegaDefs imports OmegaDefs AST Prelude begin
 
-  
-
-subsection"Production of Get and Set" 
-definition set_to_list :: "'a set \<Rightarrow> 'a list"
-  where "set_to_list s = (SOME l. set l = s)"
-    
-lemma  set_set_to_list:
-   "finite s \<Longrightarrow> set (set_to_list s) = s"
-unfolding set_to_list_def by (metis (mono_tags) finite_list some_eq_ex)    
 text{*Concatenation of two names - Used for the CSP renaming of Process+StateVariables*}
-fun abs_join_name
+fun "abs_join_name"
 where
   "abs_join_name n v = (n @ (''_'' @ v))"
 subsection "Manipulating Lists"
 
-fun abs_inListVar
+fun "abs_inListVar"
 where
   "abs_inListVar x Nil = False"
 | "abs_inListVar x [va] = (case x = va of
@@ -28,7 +19,7 @@ where
                               | _ \<Rightarrow> abs_inListVar x vst)"
 
  
-fun abs_delete_from_list
+fun "abs_delete_from_list"
 where
   "abs_delete_from_list x Nil = Nil"
 | "abs_delete_from_list x [v] = (case x = v of
@@ -50,7 +41,25 @@ fun abs_member
 where
   "abs_member x Nil = False"
 | "abs_member x (b # y) = (if x = b then True else abs_member x y)"
- 
+
+lemma thm_member: "abs_member x xs = member x xs"
+proof (induct xs)
+  case Nil
+  then show ?case by simp
+next
+  case (Cons a xs)
+  then show ?case 
+  proof (induct xs)
+    case Nil
+    then show ?case by simp
+  next
+    case (Cons a xs)
+    then show ?case by simp
+  qed
+
+qed
+  
+  
 fun abs_intersect
 where
   "abs_intersect Nil y = Nil"
@@ -81,7 +90,25 @@ where
   "abs_remdups Nil = Nil"
 | "abs_remdups (x # xs) = (if (abs_member x xs) then abs_remdups xs
                        else x # abs_remdups xs)"
-
+lemma thm_remdups: "abs_remdups x = remdups x"
+proof (induct x)
+  case Nil
+  then show ?case by auto
+next
+  case (Cons a x)
+  then show ?case 
+  proof (induct x)
+    case Nil
+    then show ?case by auto
+  next
+    case (Cons a x)
+    then show ?case
+      by (simp add: thm_member)
+  qed
+    
+qed
+  
+  
 fun abs_subset
 where
   "abs_subset xs ys = list_all (% arg0 . abs_member arg0 ys) xs"
@@ -90,87 +117,99 @@ where
 subsection"Free Variables functions" 
 fun abs_free_var_ZGenFilt
 where
-  "abs_free_var_ZGenFilt (Choose v e) = {v}"
-| "abs_free_var_ZGenFilt (Check p) = {}"
-| "abs_free_var_ZGenFilt (Evaluate v e1 e2) = {}"
+  "abs_free_var_ZGenFilt (Choose v e) = [v]"
+| "abs_free_var_ZGenFilt (Check p) = Nil"
+| "abs_free_var_ZGenFilt (Evaluate v e1 e2) = Nil"
 
  
-fun abs_free_var_ZPred :: "ZPred \<Rightarrow> ZVar set"
+fun abs_free_var_ZPred :: "ZPred \<Rightarrow> ZVar list"
 where
-  "abs_free_var_ZPred (ZFalse p) = {}"
-| "abs_free_var_ZPred (ZTrue p) = {}"
-| "abs_free_var_ZPred (ZAnd a b) = (abs_free_var_ZPred a \<union> abs_free_var_ZPred b)"
-| "abs_free_var_ZPred x = {}"
+  "abs_free_var_ZPred (ZFalse p) = Nil"
+| "abs_free_var_ZPred (ZTrue p) = Nil"
+| "abs_free_var_ZPred (ZAnd a b) = (abs_free_var_ZPred a @ abs_free_var_ZPred b)"
+| "abs_free_var_ZPred x = Nil"
 
-fun abs_fv_ZExpr
-where
-  "abs_fv_ZExpr (ZVar v) = {v}"
-| "abs_fv_ZExpr (ZInt c) = {}"
-| "abs_fv_ZExpr (ZSetDisplay x) = {y. \<exists> a \<in> (set x) . y \<in> abs_fv_ZExpr a}"
-| "abs_fv_ZExpr (ZSeqDisplay x) = {y. \<exists> a \<in> (set x) . y \<in> abs_fv_ZExpr a}"
-| "abs_fv_ZExpr (ZCall ex ex2) = abs_fv_ZExpr ex2"
-| "abs_fv_ZExpr _ = {}"
  
-fun abs_fv_CAction :: "CAction \<Rightarrow> ZVar set"
+fun abs_fvs
 where
-  "abs_fv_CAction CSPSkip = {}"
-| "abs_fv_CAction CSPStop = {}"
-| "abs_fv_CAction CSPChaos = {}"
-| "abs_fv_CAction (CSPSeq ca cb) = ((abs_fv_CAction ca) \<union> (abs_fv_CAction cb))"
-| "abs_fv_CAction (CSPExtChoice ca cb) = ((abs_fv_CAction ca) \<union> (abs_fv_CAction cb))"
-| "abs_fv_CAction (CSPCommAction v va) = {}"   
-lemma set_to_list_FV_Union: "set_to_list (abs_fv_CAction  (CSPExtChoice CSPSkip CSPSkip)) =  set_to_list (abs_fv_CAction CSPSkip) @  set_to_list (abs_fv_CAction CSPSkip) "
-  apply auto
-  by (meson finite.emptyI set_empty set_set_to_list)
-    
-lemma "set_to_list (abs_fv_CAction  (CSPExtChoice a1 a2)) =  set_to_list (abs_fv_CAction a1) @  set_to_list (abs_fv_CAction a2) "
-  apply simp
-  sorry
+  "abs_fvs f Nil = Nil"
+| "abs_fvs f (e # es) = (f e @ (abs_fvs f es))"
 
-lemma "set_to_list (abs_fv_CAction x) = free_var_CAction x " 
-proof (induct x)
-  case CSPSkip
-  then show ?case
-    using set_set_to_list by force
-next
-  case CSPStop
-  then show ?case
-    using set_set_to_list by force
-next
-  case CSPChaos
-  then show ?case
-    using set_set_to_list by force
-next
-  case (CSPCommAction x1 x)
-  then show ?case
-    using set_set_to_list by force
-next
-  case (CSPSeq x1 x2)
-  then show ?case sorry
-next
-  case (CSPExtChoice x1 x2)
-  then show ?case sorry
-qed
-  
-  
-  
-    
+ 
+function (sequential) abs_free_var_ZExpr :: "ZExpr \<Rightarrow> ZVar list"
+where
+  "abs_free_var_ZExpr (ZVar v) = [v]"
+| "abs_free_var_ZExpr (ZInt c) = Nil"
+| "abs_free_var_ZExpr (ZSetDisplay exls) = abs_fvs abs_free_var_ZExpr exls"
+| "abs_free_var_ZExpr (ZSeqDisplay exls) = abs_fvs abs_free_var_ZExpr exls"
+| "abs_free_var_ZExpr (ZCall ex ex2) = abs_free_var_ZExpr ex2"
+| "abs_free_var_ZExpr _ = Nil"
+  by pat_completeness auto
+ 
+fun abs_free_var_CAction :: "CAction \<Rightarrow> ZVar list"
+where
+  "abs_free_var_CAction CSPSkip = Nil"
+| "abs_free_var_CAction CSPStop = Nil"
+| "abs_free_var_CAction CSPChaos = Nil"
+| "abs_free_var_CAction (CSPSeq ca cb) = ((abs_free_var_CAction ca) @ (abs_free_var_CAction cb))"
+| "abs_free_var_CAction (CSPExtChoice ca cb) = ((abs_free_var_CAction ca) @ (abs_free_var_CAction cb))"
+| "abs_free_var_CAction (CSPCommAction v va) = Nil"    
+
+lemma thm_free_var_CAction: "abs_free_var_CAction c = free_var_CAction c" 
+  proof (induction c)
+    case CSPSkip
+    then show ?case by simp
+  next
+    case CSPStop
+    then show ?case by simp
+  next
+    case CSPChaos
+    then show ?case by simp
+  next
+    case (CSPCommAction x1 c)
+    then show ?case by simp
+  next
+    case (CSPSeq c1 c2)
+    then show ?case by simp
+  next
+    case (CSPExtChoice c1 c2)
+    then show ?case by simp
+  qed
+
+subsection"Production of Get and Set" 
+
 text{*We have to create signals with gets and sets carrying the values of the state variables
 before an action can occur when we translate from state-rich Circus processes into stateless ones.*}
-
 fun abs_make_get_com :: "ZName list \<Rightarrow> CAction \<Rightarrow> CAction"
 where
   "abs_make_get_com [x] c = (CSPCommAction (ChanComm ''mget'' [ChanDotExp (ZVar (x, Nil)), ChanInp (''v_'' @ x)]) c)"
 | "abs_make_get_com (x # xs) c = (CSPCommAction (ChanComm ''mget'' [ChanDotExp (ZVar (x, Nil)), ChanInp (''v_'' @ x)]) (abs_make_get_com xs c))"
 | "abs_make_get_com x c = c"
 
- 
+
+lemma thm_make_get_com: "abs_make_get_com ys c = make_get_com ys c" 
+  proof (induction ys arbitrary: c)
+    case Nil
+    then show ?case by simp
+  next
+    case (Cons a ys)
+    then show ?case 
+      proof (induct ys)
+        case Nil
+        then show ?case by simp
+      next
+        case (Cons a xs)
+        then show ?case by simp
+      qed
+  qed
+
 fun abs_make_set_com :: "(CAction \<Rightarrow> CAction) \<Rightarrow> ZVar list \<Rightarrow> ZExpr list \<Rightarrow> CAction \<Rightarrow> CAction"
 where
   "abs_make_set_com f [(x, _)] [y] c = (CSPCommAction (ChanComm ''mset'' [ChanDotExp (ZVar (x, Nil)), ChanOutExp y]) (f c))"
 | "abs_make_set_com f ((x, _) # xs) (y # ys) c = (CSPCommAction (ChanComm ''mset'' [ChanDotExp (ZVar (x, Nil)), ChanOutExp y]) (abs_make_set_com f xs ys c))"
 | "abs_make_set_com f _ _ c = (f c)"
-
+  
+  
 subsection"Production of WrtV" 
   
 fun abs_getWrtV
@@ -205,6 +244,7 @@ where
 | "abs_rename_ZExpr (ZCall xpr1 xpr2) = (ZCall (abs_rename_ZExpr xpr1) (abs_rename_ZExpr xpr2))"
 | "abs_rename_ZExpr x = x"
 
+
  
 fun abs_bindingsVar
 where
@@ -226,7 +266,26 @@ where
 | "abs_rename_vars_CAction (CSPExtChoice a1 a2) = (CSPExtChoice (abs_rename_vars_CAction a1) (abs_rename_vars_CAction a2))"
 | "abs_rename_vars_CAction x = x"
 
- 
+lemma thm_rv_CAction: "abs_rename_vars_CAction x = rename_vars_CAction x" 
+  proof (induct x)
+    case CSPSkip
+    then show ?case by simp
+  next
+    case CSPStop
+    then show ?case  by simp
+  next
+    case CSPChaos
+    then show ?case  by simp
+  next
+    case (CSPCommAction x1 x)
+    then show ?case  by simp
+  next
+    case (CSPSeq x1 x2)
+    then show ?case by simp
+  next
+    case (CSPExtChoice x1 x2)
+    then show ?case by auto
+qed
  
 fun abs_getFV
 where
--- haskabelle/thy/TestingMappingFunStatelessCircus.thy
+++ haskabelle/thy/TestingMappingFunStatelessCircus.thy
@@ -10,8 +10,8 @@ where
 | "\<Omega> CSPChaos = CSPChaos"
 | "\<Omega> (CSPSeq ca cb) = (CSPSeq (\<Omega> ca) (\<Omega> cb))"
 | "\<Omega> (CSPExtChoice ca cb) 
-    = (let lsx = (map fst (remdups (set_to_list (abs_fv_CAction (CSPExtChoice ca cb)))))
-       in make_get_com lsx (abs_rename_vars_CAction (CSPExtChoice (\<Omega>' ca) (\<Omega>' cb))))"
+    = (let lsx = (map fst (abs_remdups (abs_free_var_CAction (CSPExtChoice ca cb))))
+       in abs_make_get_com lsx (abs_rename_vars_CAction (CSPExtChoice (\<Omega>' ca) (\<Omega>' cb))))"
 | "\<Omega> x = x"
 | "\<Omega>' CSPSkip = CSPSkip"
 | "\<Omega>' CSPStop = CSPStop"
@@ -19,16 +19,31 @@ where
 | "\<Omega>' (CSPSeq ca cb) = (CSPSeq (\<Omega>' ca) (\<Omega>' cb))"
 | "\<Omega>' x = \<Omega> x"
 
-lemma "set_to_list {} = []"
-   by (meson finite.emptyI set_empty set_set_to_list)
-
-lemma "a1 = CSPSkip \<and> a2 = CSPSkip \<Longrightarrow> \<Omega> (CSPExtChoice a1 a2) = omega_CAction (CSPExtChoice a1 a2)"
-  apply simp
-  by (metis Domain_empty OmegaDefs.remdups.simps(1) finite.emptyI fst_eq_Domain make_get_com.simps(3) set_empty set_map set_set_to_list)
-
-lemma "a1 = CSPSkip \<and> a2 = CSPSkip \<Longrightarrow> \<Omega> (CSPSeq a1 a2) = omega_CAction  (CSPSeq a1 a2)"
- by simp
+lemma "a1 = CSPSkip \<and> a2 = CSPSkip \<Longrightarrow> \<Omega> (CSPExtChoice a1 a2) = omega_CAction (CSPExtChoice a1 a2)" by simp
     
+lemma "a1 = CSPSkip \<and> a2 = CSPSkip \<Longrightarrow> \<Omega> (CSPSeq a1 a2) = omega_CAction  (CSPSeq a1 a2)" by simp
+lemma thm_omega_prime:"\<Omega>' a = omega_prime_CAction a"
+proof (induction a)
+  case CSPSkip
+  then show ?case by simp
+next
+  case CSPStop
+  then show ?case  by simp
+next
+  case CSPChaos
+  then show ?case  by simp
+next
+  case (CSPCommAction x1 a)
+  then show ?case by simp
+next
+  case (CSPSeq a1 a2)
+  then show ?case  by simp
+next
+  case (CSPExtChoice a1 a2)
+  then show ?case 
+    by (simp add: thm_rv_CAction thm_make_get_com thm_free_var_CAction thm_remdups)
+qed
+      
 lemma "\<Omega> a = omega_CAction a"
 proof (induction a)
   case CSPSkip
@@ -47,7 +62,8 @@ next
   then show ?case  by simp
 next
   case (CSPExtChoice a1 a2)
-  then show ?case sorry   
+  then show ?case 
+    by (simp add: thm_omega_prime thm_rv_CAction thm_make_get_com thm_free_var_CAction thm_remdups)
 qed
   
 end
